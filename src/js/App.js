7e046534f2ed2c5d165ad6ac84a5e37be5d62365
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2023-03-06 22:58:15 +0200

    feat: Created App class

---
+++ src/js/App.js
@@ -0,0 +1,12 @@
+import * as PIXI from "pixi.js";
+
+class Application {
+    run(config) {
+        this.config = config;
+        this.app = new PIXI.Application({ resizeTo: window });
+
+        document.body.appendChild(this.app.view);
+    }
+}
+
+export const App = new Application();
