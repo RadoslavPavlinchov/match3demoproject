a9c2aed7ce25570702d10cae6b0c6d0cfe53d63e
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2023-03-11 18:43:33 +0200

    update: webpack-config
    installed loaders for CSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import "./css/styles.css";
 import { App } from "./js/App";
 import { Config } from "./js/Config";
+
 
 App.run(Config)
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
+    devtool: "eval-source-map",
     entry: "./src/index.js",
     devServer: {
         static: './dist',
     },
-    plugins: [new HtmlWebpackPlugin()],
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
