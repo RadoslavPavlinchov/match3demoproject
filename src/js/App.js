dd470d0e9cbb1b14b2fc35dcbf75c138a6dfd789
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2023-04-22 22:58:12 +0300

    feat: added a SceneManager

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,8 @@
 import * as PIXI from "pixi.js";
 import { gsap } from "gsap";
 import { PixiPlugin } from "gsap/PixiPlugin";
+import SceneManager from "./lib/SceneManager";
+import { Game } from "./scenes/Game";
 
 gsap.registerPlugin(PixiPlugin);
 PixiPlugin.registerPIXI(PIXI);
@@ -10,14 +12,21 @@ class Application {
         this.config = config;
         this.app = new PIXI.Application({ resizeTo: window });
 
+        this.scene = new SceneManager(this.app.stage);
+
         document.body.appendChild(this.app.view);
 
         this.start();
     }
 
     start() {
-        this.scene = new this.config["startScene"]();
-        this.app.stage.addChild(this.scene.container);
+        // this.scene = new this.config["startScene"]();
+        // this.app.stage.addChild(this.scene.container);
+
+        this.scene.add("Game", Game);
+        this.scene.start("Game");
+
+        console.log(this.app)
     }
 }
 
---
+++ src/js/lib/SceneManager.js
@@ -0,0 +1,32 @@
+export default class SceneManager {
+    constructor(stage) {
+        this.stage = stage;
+        this.current = null;
+    }
+
+    add(name, scene) {
+        // Add new scene
+
+        this[name] = scene;
+
+        console.log("SceneManager", this)
+    }
+
+    start(name, options) {
+        // Start current scene
+
+        // Remove any other active scenes
+        if (this.stage.children.length) {
+            this.stage.removeChildren();
+        }
+
+        // Current scene should extent PIXI.Container
+        this.current = new this[name](options);
+
+        // Add the scene/container
+        this.stage.addChild(this.current.container);
+
+
+        console.log("check stage", this)
+    }
+}
