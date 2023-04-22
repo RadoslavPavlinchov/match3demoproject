828240ad1ed668b67d7fe305195f3929df2b4249
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2023-04-22 23:54:02 +0300

    feat: created a new scene called Start
    - it will be used from players to start a new game

---
+++ src/js/scenes/Start.js
@@ -0,0 +1,13 @@
+import BaseScene from "../lib/BaseScene"
+
+export default class Start extends BaseScene {
+    constructor() {
+        super();
+
+        // this.init();
+    }
+
+    init() {
+        console.log("Start scene init")
+    }
+}
